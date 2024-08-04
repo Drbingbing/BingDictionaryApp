dcf950039dcc8799681dd96f2c58e19ebcff19d1
bingbing <tw718140@gmail.com>
2024-08-04 15:10:23 +0800

    migrate appDelegate to swiftUI

--- BingDictionary/iOS/AppDelegate.swift
+++ BingDictionary/iOS/AppDelegate.swift
@@ -2,25 +2,14 @@
 //  AppDelegate.swift
 //  BingDictionary
 //
-//  Created by Bing Bing on 2024/8/3.
+//  Created by Bing Bing on 2024/8/4.
 //
-
 import UIKit
 
-@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 }
-
---
+++ BingDictionary/iOS/DictionaryAnyWhereApp.swift
@@ -0,0 +1,19 @@
+//
+//  DictionaryAnyWhereApp.swift
+//  BingDictionary
+//
+//  Created by Bing Bing on 2024/8/3.
+//
+import SwiftUI
+
+@main
+struct DictionaryAnyWhereApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Dictionary Anywhere")
+        }
+    }
+}
